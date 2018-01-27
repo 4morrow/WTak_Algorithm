9e1491e806ffcb1ea13fbc587bb949e9401fc275
한우탁 <wtak@wtakui-macbook-pro.local>
2018-01-28 06:44:36 +0900

    Initial Commit

---
+++ WTak_Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  WTak_Algorithm
+//
+//  Created by WTak on 2018. 1. 28..
+//  Copyright © 2018년 WTak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
